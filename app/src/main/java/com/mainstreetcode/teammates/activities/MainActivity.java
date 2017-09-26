78e31cca4c293e3021a5b4c4ed67f2531e858c9f
Adetunji Dahunsi <tjdah100@gmail.com>
2017-09-26 19:39:00 -0400

    Made bottom bav bar sync up with fragment state

--- app/src/main/java/com/mainstreetcode/teammates/activities/MainActivity.java
+++ app/src/main/java/com/mainstreetcode/teammates/activities/MainActivity.java
@@ -5,7 +5,11 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.mainstreetcode.teammates.R;
 import com.mainstreetcode.teammates.baseclasses.TeammatesBaseActivity;
@@ -15,6 +19,7 @@ import com.mainstreetcode.teammates.fragments.main.EventsFragment;
 import com.mainstreetcode.teammates.fragments.main.HomeFragment;
 import com.mainstreetcode.teammates.fragments.main.SettingsFragment;
 import com.mainstreetcode.teammates.fragments.main.TeamChatFragment;
+import com.mainstreetcode.teammates.fragments.main.TeamMediaFragment;
 import com.mainstreetcode.teammates.fragments.main.TeamsFragment;
 import com.mainstreetcode.teammates.model.Event;
 import com.mainstreetcode.teammates.model.Model;
@@ -27,10 +32,36 @@ public class MainActivity extends TeammatesBaseActivity {
 
     public static final String FEED_DEEP_LINK = "feed-deep-link";
 
+    private BottomNavigationView bottomNavigationView;
+
+    final FragmentManager.FragmentLifecycleCallbacks lifecycleCallbacks = new FragmentManager.FragmentLifecycleCallbacks() {
+        @Override
+        public void onFragmentViewCreated(FragmentManager fm, Fragment f, View v, Bundle savedInstanceState) {
+            Menu menu = bottomNavigationView.getMenu();
+
+            String t = f.getTag();
+
+            if (t == null) return;
+            int id = 0;
+
+            if (t.contains(HomeFragment.class.getSimpleName())) id = R.id.action_home;
+            else if (t.contains(EventsFragment.class.getSimpleName())) id = R.id.action_events;
+            else if (t.contains(TeamChatFragment.class.getSimpleName())) id = R.id.action_messages;
+            else if (t.contains(TeamMediaFragment.class.getSimpleName())) id = R.id.action_media;
+            else if (t.contains(TeamsFragment.class.getSimpleName())) id = R.id.action_team;
+
+            if (id == 0) return;
+            MenuItem menuItem = menu.findItem(id);
+
+            if (menuItem != null) menuItem.setChecked(true);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().registerFragmentLifecycleCallbacks(lifecycleCallbacks, false);
 
         UserViewModel userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
 
@@ -41,7 +72,7 @@ public class MainActivity extends TeammatesBaseActivity {
 
         TeammatesInstanceIdService.updateFcmToken();
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this::onOptionsItemSelected);
 
         route(savedInstanceState);
@@ -53,20 +84,20 @@ public class MainActivity extends TeammatesBaseActivity {
             case R.id.action_home:
                 showFragment(HomeFragment.newInstance());
                 return true;
-            case R.id.action_settings:
-                showFragment(SettingsFragment.newInstance());
-                return true;
             case R.id.action_events:
                 showFragment(EventsFragment.newInstance());
-                return true;
-            case R.id.action_team:
-                showFragment(TeamsFragment.newInstance());
                 return true;
             case R.id.action_messages:
                 TeamPickerFragment.request(this, R.id.request_chat_team_pick);
                 return true;
             case R.id.action_media:
                 TeamPickerFragment.request(this, R.id.request_media_team_pick);
+                return true;
+            case R.id.action_team:
+                showFragment(TeamsFragment.newInstance());
+                return true;
+            case R.id.action_settings:
+                showFragment(SettingsFragment.newInstance());
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -80,7 +111,8 @@ public class MainActivity extends TeammatesBaseActivity {
 
         if (model != null) {
             if (model instanceof Event) route = EventEditFragment.newInstance((Event) model);
-            if (model instanceof TeamChat) route = TeamChatFragment.newInstance(((TeamChat) model).getTeam());
+            if (model instanceof TeamChat)
+                route = TeamChatFragment.newInstance(((TeamChat) model).getTeam());
         }
         else if (savedInstanceState == null) route = HomeFragment.newInstance();
 
