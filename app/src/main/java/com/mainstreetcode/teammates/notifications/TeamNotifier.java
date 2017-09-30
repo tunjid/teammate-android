e3f251bb07980dbe0d6590972cfaeea6fde24882
Adetunji Dahunsi <tjdah100@gmail.com>
2017-09-30 10:29:40 -0400

    Added Notifiers for missing models

---
+++ app/src/main/java/com/mainstreetcode/teammates/notifications/TeamNotifier.java
@@ -0,0 +1,38 @@
+package com.mainstreetcode.teammates.notifications;
+
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import com.mainstreetcode.teammates.R;
+import com.mainstreetcode.teammates.model.Event;
+import com.mainstreetcode.teammates.repository.EventRepository;
+import com.mainstreetcode.teammates.repository.ModelRespository;
+
+
+public class EventNotifier extends Notifier<Event> {
+
+    private static EventNotifier INSTANCE;
+
+    private EventNotifier() {
+
+    }
+
+    public static EventNotifier getInstance() {
+        if (INSTANCE == null) INSTANCE = new EventNotifier();
+        return INSTANCE;
+    }
+
+    @Override
+    protected ModelRespository<Event> getRepository() {
+        return EventRepository.getInstance();
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    @Override
+    protected NotificationChannel[] getNotificationChannels() {
+        return new NotificationChannel[]{buildNotificationChannel(FeedItem.EVENT, R.string.events, R.string.events_notifier_description, NotificationManager.IMPORTANCE_DEFAULT)};
+    }
+}
