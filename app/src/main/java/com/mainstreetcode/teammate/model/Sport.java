56f3ec3531adf336d4043c7e8f9c202f20317a67
Adetunji Dahunsi <tjdah100@gmail.com>
2018-04-10 23:46:21 -0400

    Added emoji to config

--- app/src/main/java/com/mainstreetcode/teammate/model/Sport.java
+++ app/src/main/java/com/mainstreetcode/teammate/model/Sport.java
@@ -13,14 +13,16 @@ public class Sport {
 
     private String code;
     private String name;
+    private String emoji;
 
-    Sport(String code, String name) {
+    Sport(String code, String name, String emoji) {
         this.code = code;
         this.name = name;
+        this.emoji = emoji;
     }
 
     public static Sport empty() {
-        return new Sport("", "");
+        return new Sport("", "", "\uD83E\uDD14");
     }
 
     public String getCode() {
@@ -31,6 +33,7 @@ public class Sport {
 
         private static final String CODE_KEY = "code";
         private static final String NAME_KEY = "name";
+        private static final String EMOJI_KEY = "emoji";
 
         @Override
         public Sport deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
@@ -38,8 +41,9 @@ public class Sport {
 
             String code = ModelUtils.asString(CODE_KEY, sportJson);
             String name = ModelUtils.asString(NAME_KEY, sportJson);
+            String emoji = ModelUtils.asString(EMOJI_KEY, sportJson);
 
-            return new Sport(code, name);
+            return new Sport(code, name, emoji);
         }
     }
 }
