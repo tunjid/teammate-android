a0ea3c6bc288b8493921c48e2c3f9b92eed86ce9
Adetunji Dahunsi <tjdah100@gmail.com>
2018-05-20 23:48:50 -0400

    Fixes for join request shared element transition

--- app/src/main/java/com/mainstreetcode/teammate/adapters/viewholders/JoinRequestViewHolder.java
+++ app/src/main/java/com/mainstreetcode/teammate/adapters/viewholders/JoinRequestViewHolder.java
@@ -8,6 +8,9 @@ import com.mainstreetcode.teammate.adapters.TeamMemberAdapter;
 import com.mainstreetcode.teammate.model.JoinRequest;
 import com.mainstreetcode.teammate.model.User;
 
+import static android.support.v4.view.ViewCompat.setTransitionName;
+import static com.mainstreetcode.teammate.util.ViewHolderUtil.getTransitionName;
+
 public class JoinRequestViewHolder extends ModelCardViewHolder<JoinRequest, TeamMemberAdapter.UserAdapterListener>
         implements View.OnClickListener {
 
@@ -27,6 +30,9 @@ public class JoinRequestViewHolder extends ModelCardViewHolder<JoinRequest, Team
         subtitle.setText(model.isTeamApproved() && !model.isUserApproved()
                 ? context.getString(R.string.user_invited, model.getPosition().getName())
                 : context.getString(R.string.user_requests_join, model.getPosition().getName()));
+
+        setTransitionName(itemView, getTransitionName(model, R.id.fragment_header_background));
+        setTransitionName(thumbnail, getTransitionName(model, R.id.fragment_header_thumbnail));
     }
 
     @Override
