bdb68e3c6d8b343ea0d9b2d70b95ce0110e8dfba
Adetunji Dahunsi <tjdah100@gmail.com>
2018-10-28 22:10:26 -0400

    androidx wip

--- app/src/main/java/com/mainstreetcode/teammate/model/Ad.java
+++ app/src/main/java/com/mainstreetcode/teammate/model/Ad.java
@@ -1,25 +1,21 @@
 package com.mainstreetcode.teammate.model;
 
 
-import android.graphics.drawable.Drawable;
-import androidx.annotation.Nullable;
-
-import com.google.android.gms.ads.formats.NativeAd;
-import com.google.android.gms.ads.formats.NativeAppInstallAd;
-import com.google.android.gms.ads.formats.NativeContentAd;
 import com.mainstreetcode.teammate.util.ObjectId;
 
 import java.util.List;
 
-public abstract class Ad<T extends NativeAd> implements Identifiable {
+import androidx.annotation.Nullable;
+
+public abstract class Ad<T> implements Identifiable {
 
     private T nativeAd;
     private final String id = new ObjectId().toHexString();
 
-    Ad(T nativeAd) {
-        this.nativeAd = nativeAd;
-    }
-
+    //    Ad(T nativeAd) {
+//        this.nativeAd = nativeAd;
+//    }
+//
     public abstract int getType();
 
     @Override
@@ -27,29 +23,31 @@ public abstract class Ad<T extends NativeAd> implements Identifiable {
         return id;
     }
 
-    public T getNativeAd() {
-        return nativeAd;
-    }
-
+    //
+//    public T getNativeAd() {
+//        return nativeAd;
+//    }
+//
     @Nullable
     public String getImageAspectRatio() {
-        NativeAd.Image image = getImage();
-        if (image == null) return null;
-
-        Drawable drawable = image.getDrawable();
-        if (drawable == null) return null;
-
-        return "H," + drawable.getIntrinsicWidth() + ":" + drawable.getIntrinsicHeight();
+        return "H,1:1";
+//        NativeAd.Image image = getImage();
+//        if (image == null) return null;
+//
+//        Drawable drawable = image.getDrawable();
+//        if (drawable == null) return null;
+//
+//        return "H," + drawable.getIntrinsicWidth() + ":" + drawable.getIntrinsicHeight();
     }
-
-    @Nullable
-    public NativeAd.Image getImage() {
-        List<NativeAd.Image> images = null;
-        if (nativeAd instanceof NativeContentAd) images = ((NativeContentAd) nativeAd).getImages();
-        else if (nativeAd instanceof NativeAppInstallAd)
-            images = ((NativeAppInstallAd) nativeAd).getImages();
-
-        if (images == null || images.isEmpty()) return null;
-        return images.get(0);
-    }
+//
+//    @Nullable
+//    public NativeAd.Image getImage() {
+//        List<NativeAd.Image> images = null;
+//        if (nativeAd instanceof NativeContentAd) images = ((NativeContentAd) nativeAd).getImages();
+//        else if (nativeAd instanceof NativeAppInstallAd)
+//            images = ((NativeAppInstallAd) nativeAd).getImages();
+//
+//        if (images == null || images.isEmpty()) return null;
+//        return images.get(0);
+//    }
 }
